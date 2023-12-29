8c8cc6d3d98afd7da352eb30db70c35d9e244373
shoxie <19520377@gm.uit.edu.vn>
2023-12-29 11:26:15 +0700

    unfinished lightbulb

---
+++ src/pages/bulb.tsx
@@ -0,0 +1,65 @@
+import { PiLightbulbThin } from "react-icons/pi";
+import { motion, useAnimation, useDragControls, useMotionValue, useTransform } from "framer-motion";
+import { useState } from "react";
+
+const LightBulb = () => {
+    const controls = useDragControls();
+    const y = useMotionValue(0);
+    const scale = useTransform(y, [-150, 150], [1.5, 0.5]);
+    const rotate = useTransform(y, [-150, 150], [-90, 90]);
+    const anim = useAnimation()
+
+    const [isLight, setIsLight] = useState(false)
+
+    return (
+        <motion.div
+            className="flex items-center justify-center w-screen h-screen transition-all"
+            animate={{
+                backgroundColor: isLight ? "black" : "white",
+                transition: {
+                    duration: 0.6
+                }
+            }}
+        >
+            <div className="text-9xl relative flex flex-col items-center">
+                <motion.div initial={{
+                    pathLength: 0
+                }} 
+                animate={{
+                    color: isLight ? "yellow" : "black",
+                    pathLength: 1
+                }}>
+                    <PiLightbulbThin />
+                </motion.div>
+                <motion.div
+                    id="string"
+                    className="w-1 h-40 bg-gray-200 absolute top-full"
+                    drag="y"
+                    dragControls={controls}
+                    style={{
+                        y,
+                        scale,
+                    }}
+                    animate={anim}
+                    dragConstraints={{ left: 0, right: 0 }}
+                    dragTransition={{ bounceStiffness: 600, bounceDamping: 20 }}
+                    whileTap={{ cursor: "grabbing" }}
+                    onDragEnd={async () => {
+                        anim.start({
+                            y: 0,
+                            transition: {
+                                bounceStiffness: 600,
+                                bounceDamping: 20,
+                                type: "spring"
+                            }
+                        })
+                        setIsLight(!isLight)
+                    }}
+                    dragSnapToOrigin
+                ></motion.div>
+            </div>
+        </motion.div>
+    );
+};
+
+export default LightBulb;
---
+++ src/pages/transition.tsx
@@ -0,0 +1,77 @@
+'use client'
+import { motion, useAnimation } from "framer-motion";
+import { useState } from "react";
+
+const PageTransition = () => {
+    const [open, setOpen] = useState(false);
+    const control = useAnimation();
+
+    const toggle = () => {
+        setOpen(prev => !prev)
+        animate(open)
+    }
+
+    const animate = async (isInitial: boolean) => {
+        if (!isInitial) {
+            await control.start({
+                height: "100vh",
+                width: "100vw",
+                transition: {
+                    duration: 1
+                }
+            })
+    
+            await control.start({
+                background: "green",
+                transition: {
+                    duration: 1,
+                    ease: "easeInOut",
+                    type: "spring"
+                }
+            })
+        } else {
+            await control.start({
+                height: "auto",
+                width: "50vw",
+                transition: {
+                    duration: 1
+                }
+            })
+    
+            await control.start({
+                background: "red",
+                transition: {
+                    duration: 1,
+                    ease: "easeInOut",
+                    type: "spring"
+                }
+            })
+        }
+    }
+
+    return (
+        <div className="relative w-screen h-screen">
+            <motion.div
+                key={"page"}
+                layout
+                initial={{
+                    height: "auto",
+                    // top: "100%"
+                }}
+                // animate={{
+                //     height: open ? "100vh" : "auto",
+                //     top: open ? 0 : "90%",
+                //     background: open ? "green" : "red",
+                //     transition: {
+                //         duration: 1.7
+                //     }
+                // }}
+                animate={control}
+                className="absolute left-0 bg-red-200">
+                <button onClick={toggle}>click me to show</button>
+            </motion.div>
+        </div>
+    )
+}
+
+export default PageTransition
